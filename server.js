7eec235dde5b3e58f8033a919c11aa543efdc3c0
Amanda Allred <amanda.barnett11@yahoo.com>
2025-10-15 14:28:54 -0400

    testing call of API key

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ const app = express();
 // Store port number 
 const port = 8080;
 
+// Include packages
+require('dotenv').config();
+const cors = require('cors');
+
+app.use(cors());
+
 // Static files are all in public folder; html, css, client-side js
 // Middleware function
 app.use(express.static("./public"));
@@ -16,6 +22,29 @@ app.use(express.static("./public"));
 
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+
+
+async function getRandomImage() {
+    const endpoint = `https://api.unsplash.com/photos/random/?client_id=${process.env.CLIENT_ID}`;
+    try {
+        const response = await fetch(endpoint);
+        const returnedData = await response.json();
+        const receivedPhotoUrl = returnedData.urls.regular;
+
+        return receivedPhotoUrl;
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+
+// Create route on back-end
+app.use('/api/v1/getRandomImage', (request, response) => {
+    response.status(200).json({
+        status: 200,
+        data: process.env.CLIENT_ID
+    });
+});
 
 
 // Press Ctrl and click the link to open the server when we start running the program.
